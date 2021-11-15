83d38410d493d86694a5c14606f93aaeb8dede94
SaSa <samuel16anis@gmail.com>
2021-11-15 19:53:35 +0200

    flutter2

--- lib/main.dart
+++ lib/main.dart
@@ -47,8 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
       body: Center(
