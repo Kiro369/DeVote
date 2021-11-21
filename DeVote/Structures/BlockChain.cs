dfa1d76fb47eab4b1cefb1809fd3a00630e8078d
mahmoudElmaandi <93646241+mahmoudElmaandi@users.noreply.github.com>
2021-11-21 18:15:47 +0200

    Added functionality to save a block into leveldb

--- DeVote/Structures/BlockChain.cs
+++ DeVote/Structures/BlockChain.cs
@@ -1,17 +1,28 @@
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace DeVote.Structures
 {
     public class BlockChain
     {
         public LinkedList<Block> VChain { set; get; }
+        public LevelDb levelDb;
 
         public BlockChain()
         {
             VChain = new LinkedList<Block>();
-            Block GenesisBlock = new Block(DateTime.Now,null, null);
+            Block GenesisBlock = new Block(DateTime.Now, null, null);
             VChain.AddFirst(GenesisBlock);
+
+            string parent = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
+            string dbPath = parent+"\\dbTest";
+
+            // Create a new leveldb
+            LevelDb myLevelDb = new LevelDb(dbPath);
+            this.levelDb = myLevelDb;
+            this.levelDb.db.Put(GenesisBlock.Height.ToString(), JsonConvert.SerializeObject(GenesisBlock));
         }
 
         public void AddBlock(Block block)
@@ -21,6 +32,13 @@ namespace DeVote.Structures
             block.PrevHash = latestBlock.Hash;
             block.Hash = block.ComputeHash();
             VChain.AddLast(block);
+        }
+
+        // Save block into leveldb
+        public void saveBlock(Block block)
+        {
+            // Put each block as key-value pair where key is the position (height) of the block
+            this.levelDb.db.Put(block.Height.ToString(), JsonConvert.SerializeObject(block));
         }
     }
 }
