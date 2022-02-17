db1a9aeb00ce4d1038899f7c9813dd0932a777dc
Ghazal20 <42018105@hti.edu.eg>
2022-02-17 13:43:09 +0200

    update crop method

--- ID_OCR/id_reader.py
+++ ID_OCR/id_reader.py
@@ -1,6 +1,7 @@
 import numpy as np
 from pytesseract import image_to_string
 import cv2
+import imutils
 import re
 
 
@@ -15,22 +16,22 @@ def id_read(img, gray=False, *, data, face):
     '''
     image = img if gray else cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     image = cv2.resize(image, (1410, 900))
-    thresh = 80
+    thresh = 91
     bi_img = cv2.threshold(image, thresh, 255, cv2.THRESH_BINARY)[1]
 
-    #bi_img = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 201, 30)
+    #bi_img = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 201, 70)
     data_coords = {"front": {"first_name": [(900, 220), (1380, 300)],
                       "full_name" : [(480, 310), (1380, 400)],
                       "address"   : [(480, 420), (1380, 570)],
                       "ID"        : [(620, 685), (1380, 780)]},
-            "back": {"expire_date": [(450,350), (750,500)]}}
+            "back": {"expire_date": [(450, 350), (750, 500)]}}
 
 
-    for coord in data[face].values():
+    for coord in data_coords[face].values():
         bi_img = cv2.rectangle(bi_img, coord[0], coord[1], (0, 0, 0), 2)
 
 
-    for key, val in data[face].items():
+    for key, val in data_coords[face].items():
         (x1, y1), (x2, y2) = val
         if key == "ID" or key == "expire_date":
             data[face][key] = "".join(image_to_string(bi_img[y1:y2, x1:x2], lang="hin").split());continue
@@ -42,7 +43,9 @@ def id_read(img, gray=False, *, data, face):
 
 
 def get_contours(img_edges):
-    contours = cv2.findContours(img_edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
+    contours = cv2.findContours(img_edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(contours)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
 
     for c in contours:
         approx = cv2.approxPolyDP(c, .01 * cv2.arcLength(c, True), True)
@@ -60,8 +63,7 @@ def id_crop(id_img, gray=False):
     :return:
     '''
     image = id_img if gray else cv2.cvtColor(id_img, cv2.COLOR_BGR2GRAY)
-    image = cv2.threshold(image, 120, 255, cv2.THRESH_BINARY)[1]
-    blurred = cv2.blur(image, (5, 5))
+    blurred = cv2.bilateralFilter(image, 33, 40, 40)
     canny = cv2.Canny(blurred, 70, 200)
     contours = get_contours(canny)
     if list(contours):
