f089d241ac5f89df949cabe513fc329629ccc81a
samuel2202-anis <65362048+samuel2202-anis@users.noreply.github.com>
2022-07-14 17:49:23 +0200

    api

--- FlutterApp/lib/models/Ip.dart
+++ FlutterApp/lib/models/Ip.dart
@@ -3,33 +3,34 @@
 import 'call_api.dart';
 
 class ip {
+  static String endpoint='devote-explorer-backend.herokuapp.com';
   //for transactions in blockchain page
   callApi network = callApi(
-      Uri.https('devote-explorer-backend.herokuapp.com', 'transactions'));
+      Uri.https(endpoint, 'transactions'));
 
   //for blocks in blockchain page
   callApi block =
-  callApi(Uri.https('devote-explorer-backend.herokuapp.com', 'blocks'));
+  callApi(Uri.https(endpoint, 'blocks'));
 
   //for transactionlist showmore fn
-  String authority = 'devote-explorer-backend.herokuapp.com';
+  String authority = endpoint;
   String unencodedpath = 'transactions';
 
   //for blocklist showmore fn
-  String authority_block = 'devote-explorer-backend.herokuapp.com';
+  String authority_block = endpoint;
   String unencodedpath_block = 'blocks';
 
   //for TransactionDetails show block height
-  String authority_blockHe = 'devote-explorer-backend.herokuapp.com';
+  String authority_blockHe = endpoint;
   String unencodedpath_blockhe = 'blocks/block-height/';
 
   //for BlockDetails show block height
-  String authority_Height = 'devote-explorer-backend.herokuapp.com';
+  String authority_Height = endpoint;
   String unencodedpath_height = 'transactions/tx-block-height/';
 
   //for results candidates and map
   callApi candidates = callApi(
-      Uri.https('devote-explorer-backend.herokuapp.com', 'candidates'));
+      Uri.https(endpoint, 'candidates'));
   callApi model = callApi(
-      Uri.https('devote-explorer-backend.herokuapp.com', 'governorates'));
+      Uri.https(endpoint, 'governorates'));
 }
